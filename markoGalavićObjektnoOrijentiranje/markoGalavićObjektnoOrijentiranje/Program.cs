332b60be7b1243941c18742f2aef0e9a712dbcbb
markogalavic <55134792+markogalavic@users.noreply.github.com>
2020-03-10 11:44:14 +0100

    Add files via upload

---
+++ markoGalavićObjektnoOrijentiranje/markoGalavićObjektnoOrijentiranje/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace markoGalavićObjektnoOrijentiranje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float ProsječnaVisinaKata;
+           
+            Neboder Tower = new Neboder(200, 100);
+
+            Tower.setVisina(Tower.getVisina() + 10);
+            Tower.setBrojKatova(Tower.getBrojKatova() + 1);
+
+            ProsječnaVisinaKata = Tower.getVisina() / Tower.getBrojKatova();
+            Console.WriteLine("Prosjecna visina kata je:" + ProsječnaVisinaKata);
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
